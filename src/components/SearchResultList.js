0f880cf2b4c61b90f8db1860562fe082c21a80e1
mattjohannsen <majohann@hotmail.com>
2020-05-18 09:44:04 -0500

    Successful GET axios API call returning Object from youtube on SearchResultList.js

--- src/components/SearchResultList.js
+++ src/components/SearchResultList.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import SearchResult from './SearchResult';
+import axios from 'axios';
 
 export default function SearchResultList( props ) {
     const {
       search
     } = props;
+
+    useEffect(() => {
+        axios
+          .get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=fishing&type=video&key=${process.env.REACT_APP_GOOGLE_API_KEY}`)
+          .then(response => {
+            console.log(response.data)
+          });
+      }, []);
 
     return (
         <div className="search-result-list">
